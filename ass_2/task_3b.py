8c38cc9e927e7c1ccd2048283d498581a6b730fe
Ziad Albitar <ziad@student-232-95.eduroam.uu.se>
2025-11-14 19:09:14 +0100

    Finished code, need to label

--- ass_2/task_3b.py
+++ ass_2/task_3b.py
@@ -1,10 +1,11 @@
+import enum
 import numpy as np
 import matplotlib.pyplot as plt
 
 π = np.pi
 a = 1000 * np.pi
 w0 = 2 * π / 0.005 
-N = int(15000 / w0)
+N = int(30000 / w0)
 n = np.arange(-N, N + 1, 1)
 wn = np.arange(-N, N + 1, 1)* w0
 Xn = np.zeros_like(wn, dtype=complex)
@@ -19,18 +20,21 @@ for i in range(len(n)):
 
 Yn = Xn * Hn
 Ns = [np.arange(-1, 2), np.arange(-2, 3), np.arange(-10, 11), np.arange(-20, 21)]
-y = np.zeros(4)
-t = np.arange(0, 16)
+t = np.arange(0, 0.015, 0.0001)
+y = np.zeros((4, len(t)), dtype=complex)
 
-for N in Ns:
+for i, N_range in enumerate(Ns):
     sum = 0
-    for n in N:
-        sum += Yn[int(n)] * np.exp(1j * n * w0 * t)
-    y[n] = (sum * (1/(2*π)))
+    for n in N_range:
+        index = N + n 
+        sum += Yn[index] * np.exp(1j * n * w0 * t)
+    sum = (sum * (1/(2*π)))
+    y[i] = sum
 
-plt.plot(wn, y[0])
-plt.plot(wn, y[1])
-plt.plot(wn, y[2])
-plt.plot(wn, y[3])
+plt.plot(t, y[0], label="N=1")
+plt.plot(t, y[1], label="N=2")
+plt.plot(t, y[2], label="N=10")
+plt.plot(t, y[3], label="N=20")
+plt.legend()
 plt.show()
 
